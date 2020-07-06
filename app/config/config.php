76393577fb8abad1010979bec9c3f1d7725c321c
Carol <carol@pop-os.localdomain>
2020-07-06 18:52:18 -0300

    estrutura

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE', 'quase-tudo-gostoso');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'qt_gostoso');
+
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once('app/config/config.php');
+require_once('app/function/functions.php');
+require_once('vendor/autoload.php');
